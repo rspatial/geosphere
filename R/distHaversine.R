6b4a6b2380b574ac999bc6bb0b950f0acdd9dc0b
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-09-24 08:16:19 -0700

    more haver

--- R/distHaversine.R
+++ R/distHaversine.R
@@ -8,6 +8,8 @@
 # version 0.1
 # license GPL3
 
+
+
 distHaversine <- function(p1, p2, r=6378137) {
 #* Haversine formula to calculate distance between two points specified by 
 #* from: Haversine formula - R.W. Sinnott, "Virtues of the Haversine",
@@ -29,7 +31,7 @@ distHaversine <- function(p1, p2, r=6378137) {
 		
 	dLat <- p[,4]-p[,2]
 	dLon <- p[,3]-p[,1]
-	a <- sin(dLat/2) * sin(dLat/2) + cos(p[,2]) * cos(p[,4]) * sin(dLon/2) * sin(dLon/2)
+	a <- (sin(dLat/2))^2 + cos(p[,2]) * cos(p[,4]) * (sin(dLon/2))^2
 	# to avoid values of 'a' that are a sliver above 1
 	# which may occur at antipodes
 	# https://stackoverflow.com/questions/45889616/why-does-disthaversine-return-nan-for-some-pairs-of-coordinates#
@@ -48,4 +50,42 @@ distHaversine <- function(p1, p2, r=6378137) {
 #	a <- sin(dLat/2) * sin(dLat/2) + cos(lat1) * cos(lat2) * sin(dLon/2) * sin(dLon/2)
 #	dist <- 2 * atan2(sqrt(a), sqrt(1-a)) * r
 
+
+.distHaversine2 <- function(p1, p2, r=6378137) {
+## following wikipedia
+	toRad <- pi / 180 
+	p1 <- .pointsToMatrix(p1) * toRad
+	if (missing(p2)) {
+		p2 <- p1[-1, ,drop=FALSE]
+		p1 <- p1[-nrow(p1), ,drop=FALSE]
+	} else {
+		p2 <- .pointsToMatrix(p2)  * toRad	
+	}
+
+	p = cbind(p1[,1], p1[,2], p2[,1], p2[,2], as.vector(r))
+		
+	dLat <- p[,4]-p[,2]
+	dLon <- p[,3]-p[,1]
+	a <- (sin(dLat/2))^2 + cos(p[,2]) * cos(p[,4]) * (sin(dLon/2))^2
+	a <- pmin(a, 1)
+
+	dist <- 2 * r * asin(sqrt(a))
+	return( as.vector(dist))
+}
+
+
+# from Thierry de Meeus
+.distHaversine3 <- function(p1, p2, r=6378137) {
+	toRad <- pi / 180 
+	p1 <- .pointsToMatrix(p1) * toRad
+	if (missing(p2)) {
+		p2 <- p1[-1, ,drop=FALSE]
+		p1 <- p1[-nrow(p1), ,drop=FALSE]
+	} else {
+		p2 <- .pointsToMatrix(p2)  * toRad	
+	}
+
+	p <- cbind(p1[,1], p1[,2], p2[,1], p2[,2], as.vector(r))
+	r*(pi/2-asin(sin((p[,4]))*sin((p[,2]))+cos((p[,3])-(p[,1]))*cos((p[,4]))*cos((p[,2]))))
+}
 
